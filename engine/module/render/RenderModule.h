4717f7b9c510757810e5ccce8c0f8098aeb3248e
xuhui <good122000@qq.com>
2016-04-16 23:06:01 +0800

    logic and render in seperate thread

---
+++ engine/module/render/RenderModule.h
@@ -0,0 +1,14 @@
+//
+//  RenderModule.hpp
+//  Wukong
+//
+//  Created by Xuhui on 16/4/14.
+//  Copyright © 2016年 Xuhui. All rights reserved.
+//
+
+#ifndef RenderModule_hpp
+#define RenderModule_hpp
+
+#include <stdio.h>
+
+#endif /* RenderModule_hpp */
---
+++ engine/module/render/RenderModule_LuaBinding.h
@@ -0,0 +1,14 @@
+//
+//  RenderModule_LuaBinding.hpp
+//  Wukong
+//
+//  Created by Xuhui on 16/4/14.
+//  Copyright © 2016年 Xuhui. All rights reserved.
+//
+
+#ifndef RenderModule_LuaBinding_hpp
+#define RenderModule_LuaBinding_hpp
+
+#include <stdio.h>
+
+#endif /* RenderModule_LuaBinding_hpp */
