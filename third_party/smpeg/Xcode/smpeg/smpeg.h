f33322fbd75824da870f94cfa67d360c27c9c277
xuhui <good122000@qq.com>
2016-04-18 08:55:18 +0800

    add SDL_mixer smpeg

---
+++ third_party/smpeg/Xcode/smpeg/smpeg.h
@@ -0,0 +1,13 @@
+//
+//  smpeg.h
+//  smpeg
+//
+//  Created by Xuhui on 16/4/17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface smpeg : NSObject
+
+@end
---
+++ third_party/smpeg/Xcode/smpeg/smpeg.m
@@ -0,0 +1,13 @@
+//
+//  smpeg.m
+//  smpeg
+//
+//  Created by Xuhui on 16/4/17.
+//
+//
+
+#import "smpeg.h"
+
+@implementation smpeg
+
+@end
